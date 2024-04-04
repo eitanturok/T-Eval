d3cf4b0fddb7215fa41092b3c5e637dd4647d1d8
Eitan Turok <eitan.turok@databricks.com>
2024-04-04 15:44:32 +0000

    remove unused functions

--- teval/utils/download_dataset.py
+++ teval/utils/download_dataset.py
@@ -4,18 +4,7 @@ from typing import Optional
 from huggingface_hub import snapshot_download
 
 
-def add_suffix_to_filenames(directory, suffix):
-    """Add suffix `suffix` to all the files in `directory`."""
-    for filename in os.listdir(directory):
-        if os.path.isfile(os.path.join(directory, filename)):
-            name, ext = os.path.splitext(filename)
-            new_filename = f"{name}{suffix}{ext}"
-            os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
-
-
 def download_dataset(dataset_path: Optional[str] = None):
-
-    print(dataset_path)
 
     # if `None`, download all the files in the `data` directory on HF
     if dataset_path is None:
@@ -33,9 +22,3 @@ def download_dataset(dataset_path: Optional[str] = None):
         allow_patterns=dataset_path,
         local_dir_use_symlinks=False
         )
-
-    # # the `T-eval` repo expects all filenames to end in "_suffix"
-    # data_dir = cwd + "/data"
-    # suffix = "_subset"
-    # add_suffix_to_filenames(data_dir, suffix)
-
